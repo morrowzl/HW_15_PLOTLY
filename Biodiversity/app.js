ea5af4b765f3c1869d06b1e0612dfb2e5bd3bcd2
Zachary Morrow <morrowzl@vcu.edu>
2020-01-19 00:26:06 -0500

    almost done

--- Biodiversity/app.js
+++ Biodiversity/app.js
@@ -1,7 +1,10 @@
-/*
-1. use D3 to read in the data from samples.json
-*/
 var url = "./samples.json";
+
+// 1. declare initiation function
+// select dropdown
+// use D3 to get json from data source
+// access the data
+// create dropdown option for each sample id
 
 function init() {
 
@@ -17,195 +20,105 @@ function init() {
       d3.select("#selDataset").append("option").text(`${testSub}`);
     });
 
-    var zerothSample = namesArr[0];
+    var newSample = namesArr[0];
+    let metObj = metArr.filter(obj => obj.id == newSample);
+    let samplesObj = samplesArr.filter(obj => obj.id == newSample);
+    
+    var myObj = {};
+    myObj.id = newSample;
+    myObj.metadata = metObj[0];
+    myObj.samples = samplesObj[0];
+    
+    updateVisuals(myObj);
 
-    let matchobjArr = samplesArr.filter(obj => obj.id == zerothSample);
-
-    //update charts and metadata by calling functions and passing zerothSample
-    //functions: updateCharts, updateMeta
   });
 
 }
 
+// 2. declare sampleSelected function
+// select dropdown
+// event handler: on change of select attr "value" do the following
+// use D3 to get data
+// access data
+// updateVisuals
+
+// 3. declare updateVisuals function
+// bar chart
+// bubble chart
+// metadata
+ 
+
+
+// 4. initialize page with first sample
 init();
 
+function updateVisuals(myObj) {
 
-//let newsubjectArr = samplesArr.map(sampleObject.id == newSubject, => a.foo);
+  //console.log(myObj.id);
+  //console.log(myObj.metadata);
+  //console.log(myObj.samples);
+  //console.log(myObj.samples.otu_ids);
+  //console.log(myObj.samples.sample_values);
+  //let testArr = myObj.samples.otu_ids.slice(0,10);
+  //console.log(testArr);
+  var otuArr = myObj.samples.otu_ids.slice(0,10);
+  var otuStrArr = [];
+  otuArr.map(otu => {
+    var strOTU = `OTU ${String(otu)}`;
+    otuStrArr.push(strOTU);
+  });
 
-/*
-function updateCharts(newSubject) {
-
-  var trace1 = {
+  var barTrace = {
     type: "bar",
-    x: 
-    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    y: 
-    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    labels: 
-    //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    text: 
-    //["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    x: myObj.samples.sample_values.slice(0,10),
+    y: otuStrArr,
+    labels: myObj.samples.otu_ids.slice(0,10),
+    text: myObj.samples.otu_labels.slice(0,10),
     orientation: "h"
   }
 
-  var data = [trace1];
+  var data = [barTrace];
 
   var layout = {
-    title: "test title",
-    xaxis: {title:"test xaxis title"},
-    yaxis: {title:"test yaxis title"}
+    title: `Top 10 OTUs in Subject ${myObj.id}`,
+    xaxis: {title:"OTU Population (arb.)"},
+    yaxis: {title:""},
+    bargap: 0.1
   };
 
   Plotly.newPlot("bar", data, layout);
-}
-}
 
-//function updateMeta(newValue) {}
-
-function optionChanged(newValue) {
-
-  var trace1 = {
-    type: "bar",
-    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    labels: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    text: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    orientation: "h"
+  var bubbleTrace = {
+    type
   }
+} 
 
-  var data = [trace1];
 
-  var layout = {
-    title: "test title",
-    xaxis: {title:"test xaxis title"},
-    yaxis: {title:"test yaxis title"}
-  };
+  //select dropdown
+  //get value (test subject id number)
+  //filter data objects to select the ones with the correct subject id
+  //
 
-  Plotly.newPlot("bar", data, layout);
-}
+
+
+  //id: integer
+  //ethnicity: string
+  //gender: string
+  //age: integer
+  //location: string
+  //bbtype: string
+  //wfreq: integer
+  //OR NULL
 
 // user selects test subject id number
 //d3.select("#selDataset").select("value").on("change", testFunc());
 // d3.select("#selDataset").select("value").on("change", updatePlotly);
 
-// d3.select("#selDataset").selectAll("option").remove();
 
-// function optionChanged(value) {
-//}
-*/
-  /*
-  2. Create a horizontal bar chart with a dropdown menu to display 
-  the top 10 OTUs found in that individual.
-  */
-  
-//  var x = [];
-//  var y = [];
-
-//  var selectedOption = dropdownMenu.value;
-
-//  var subjectId = selectedOption;
-  //alternative var subjectId = d3.select("#selDataset").html(this.value);
-
-//  var subjectBacteria = samplesArr[i].otu_ids.slice(0,10)
-  /*where samplesArr.[object_i].id == subjectId*/
-
-//  var bacteriaValues = samplesArr[i].sample_values.slice(0,10);
-
-//  var bacteriaLabels = samplesArr[i].otu_labels.slice(0,10);
-
-  // needs to be for specific test subject selected by user in dropdown
-  // WHERE/FOR 
-  //userInput [some number string like "940"] == samplesArr[i].id 
-
-  /*  var trace1 = {
-    type: "bar",
-    // top 10 OTUs for single individual
-    x: subjectBacteria,
-    y: bacteriaValues,
-    labels: subjectBacteria,
-    text: bacteriaLabels,
-    orientation: "h"
-  };
-
-  var data = [trace1];
-
-  var layout = {
-    title: "test title",
-    xaxis: {title:"test xaxis title"},
-    yaxis: {title:"test yaxis title"}
-  };
-
-  Plotly.newPlot("bar", data, layout);
-
-
-});
-  // Call function to update the chart
-  updatePlotly(data);
-}
-
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
-
-// Promise Pending
+/*
+Promise Pending
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
-
-/*
-function init() {
-  var trace1 = {
-    type: "bar",
-    x: [],
-    y: [],
-    orientation: "h",
-    labels: [],
-    text: []
-  };
-
-  var data = [trace1];
-  
-  Plotly.newPlot("bar", data);
-}
-*/
-
-
-/*
-Create a bubble chart that displays each sample
-*/
-/*
-var trace1 = {
-  x: [1, 2, 3, 4],
-  y: [10, 11, 12, 13],
-  mode: 'markers',
-  marker: {
-    size: [40, 60, 80, 100]
-  }
-};
-
-var data = [trace1];
-
-var layout = {
-  title: 'Marker Size',
-  showlegend: false,
-  height: 600,
-  width: 600
-};
-
-Plotly.newPlot("bubble", data, layout);
-
-/*
-4. Display the sample metadata, 
-i.e., an individual's demographic information
-*/
-
-/*
-5. Display each key-value pair from the 
-metadata JSON object somewhere on the page
-*/
-
-/*
-6. Update all of the plots any time a new sample is selected
 */
 
 /*
